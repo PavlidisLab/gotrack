PavlidisLab/gotrack

commit f58d99c2489c11a46173896325d181d67a700c8a

Fixes bugs from updating servlet/primefaces versions & styling
Includes:

Dialog rendering changed, required showing dialogs before rendering
contents.

Upgrade causing server to javascript JSON transfer to convert null into
undefined. Was causing issues when displaying rank enrichment plots,
fixed but could pop up elsewhere?

Style bugs fixed (particularly in input boxes).

A few styling fixes, including reset zoom button and not showing point
markers on plots. Resolves #2.

gotrack/src/main/webapp/resources/js/enrichment.js
@@ -172,10 +172,10 @@ function handleEnrichmentComplete(xhr, status, args) {
                      zoomType: 'x',
                      resetZoomButton: {
                         position: {
-                           // align: 'right', // by default
+                           align: 'left',
                            // verticalAlign: 'top', // by default
-                           x: -10,
-                           y: -30
+                           x: 0,
+                           y: -35,
                         }
                      },
                      events: {
@@ -335,12 +335,12 @@ function handleGraphSelected(xhr, status, args) {
                     zoomType: 'xy',
                     resetZoomButton: {
                        position: {
-                          // align: 'right', // by default
+                          align: 'left',
                           // verticalAlign: 'top', // by default
-                          x: -10,
-                          y: -30
+                          x: 0,
+                          y: -35,
                        }
-                    }
+                    },
                  };
    options.title = { text: args.hc_title };
    options.subtitle = {
@@ -663,7 +663,7 @@ function handleGraphSelected(xhr, status, args) {
 
       for (var j = 0; j < series.data.length; j++) {
          var point = series.data[j];
-         data.push([point.x,point.y]);
+         data.push({x:point.x,y:utility.isUndefined( point.y ) ? null : point.y, marker:{enabled:false}});
       }
       options.series.push({
          name : name,
@@ -681,7 +681,9 @@ function handleGraphSelected(xhr, status, args) {
 
       for (var j = 0; j < series.data.length; j++) {
          var point = series.data[j];
-         data.push([point.x,point.y.left, point.y.right]);
+         // We disabled markers on the point level because disabling it at the series level removes series symbols from the legend.
+         // This is the least intrusive way of getting around this.
+         data.push({x:point.x,y:utility.isUndefined( point.y ) ? null : point.y, marker:{enabled:false}});
       }
       
       options.series.push({

gotrack/src/main/webapp/enrichment.xhtml
@@ -315,7 +315,7 @@
 										value="Graph Selected Term"
 										actionListener="#{enrichmentView.createStabilityChartFromSelected()}"
 										icon="fa fa-line-chart"
-										oncomplete="handleGraphSelected(xhr, status, args);PF('enrichmentChartWdg').show();" />
+										oncomplete="PF('enrichmentChartWdg').show();handleGraphSelected(xhr, status, args);" />
 								</span>
 								<h:commandLink>
 									<h:graphicImage library="img" name="excel.png" width="24" />
@@ -476,7 +476,7 @@
 										value="GO"
 										actionListener="#{enrichmentView.createChart()}"
 										icon="fa fa-line-chart"
-										oncomplete="handleGraphSelected(xhr, status, args);PF('enrichmentChartWdg').show();" />
+										oncomplete="PF('enrichmentChartWdg').show();handleGraphSelected(xhr, status, args);" />
 								</span>
 								<h:commandLink>
 									<h:graphicImage library="img" name="excel.png" width="24" />

gotrack/src/main/webapp/genes.xhtml
@@ -137,25 +137,41 @@
 		<p:outputPanel rendered="#{geneView.gene == null}">
 			<h:form>
 				<h3 style="text-align: center;">Track a Gene</h3>
-				<h:panelGrid columns="2" style="margin-bottom:10px" cellpadding="5" styleClass="geneSearchTable">
-
-					<h:outputText value="Select the species of interest (required): " />
-				<p:selectOneMenu id="selectspecies"
-					value="#{geneSearchView.speciesId}" style="margin-right:5px;">
-					<p:ajax event="change" process="@this" />
-					<f:selectItems value="#{cache.speciesList}" var="spec"
-						itemValue="#{spec.id}" itemLabel="#{spec.commonName}" />
-				</p:selectOneMenu>
-				<h:outputText value="Write the gene symbol to visualize: " />
-				<p:autoComplete id="geneInput" value="#{geneSearchView.query}"
-					completeMethod="#{geneSearchView.complete}" queryDelay="600"
-					minQueryLength="2" forceSelection="true" label="ID"
-					emptyMessage="No gene suggestions available, please try again."
-					var="gene" itemLabel="#{gene.selectedGene.symbol}" itemValue="#{gene.selectedGene.symbol}"
-					groupBy="#{gene.type.label}" style="margin-right:8px;"/>
-				<p:commandButton type="submit" value="Go"
-					action="#{geneSearchView.go}" />
-					</h:panelGrid>
+				<p:panelGrid style="margin-bottom:10px" cellpadding="5" styleClass="geneSearchTable no-border">
+					<p:row>
+						<p:column>
+							<h:outputText value="Select the species of interest: " />
+						</p:column>
+						<p:column>
+							<p:selectOneMenu id="selectspecies"
+								value="#{geneSearchView.speciesId}" style="margin-right:5px;">
+								<p:ajax event="change" process="@this" />
+								<f:selectItems value="#{cache.speciesList}" var="spec"
+									itemValue="#{spec.id}" itemLabel="#{spec.commonName}" />
+							</p:selectOneMenu>
+						</p:column>
+					</p:row>
+					<p:row>
+						<p:column>
+							<h:outputText value="Write the gene symbol to visualize: " />
+						</p:column>
+						<p:column>
+							<p:autoComplete id="geneInput" value="#{geneSearchView.query}"
+								completeMethod="#{geneSearchView.complete}" queryDelay="600"
+								minQueryLength="2" forceSelection="true" label="ID"
+								emptyMessage="No gene suggestions available, please try again."
+								var="gene" itemLabel="#{gene.selectedGene.symbol}"
+								itemValue="#{gene.selectedGene.symbol}"
+								groupBy="#{gene.type.label}" style="margin-right:8px;" />
+						</p:column>
+					</p:row>
+					<p:row>
+						<p:column colspan="2">
+							<p:commandButton type="submit" value="Go"
+								action="#{geneSearchView.go}" />
+						</p:column>
+					</p:row>
+				</p:panelGrid>
 			</h:form>
 			 
 		</p:outputPanel>
@@ -182,19 +198,19 @@
 				oncomplete="handleFilterCharts(xhr, status, args);"
 				actionListener="#{geneView.filterCharts()}" />
 			<p:remoteCommand name="fetchTimeline"
-				oncomplete="handleFetchTimeline(xhr, status, args);PF('timelineDlgWdg').show();"
+				oncomplete="PF('timelineDlgWdg').show();handleFetchTimeline(xhr, status, args);"
 				actionListener="#{geneView.fetchTimeline()}" />
 			<p:remoteCommand name="fetchAnnotationPointData"
-				oncomplete="PF('clickTermsTableWdg').clearFilters();PF('clickTermsDlgWdg').show();"
+				oncomplete="handleDatatableDialog('clickTermsDlgWdg', 'clickTermsTableWdg', false);"
 				update=":clickTermsForm:clickTermsTable"
 				actionListener="#{geneView.fetchAnnotationPointData()}" />
 			<p:remoteCommand name="fetchLossGainPointData"
-				oncomplete="PF('clickLGTermsTableWdg').clearFilters();PF('clickLGTermsDlgWdg').show();"
+				oncomplete="handleDatatableDialog('clickLGTermsDlgWdg', 'clickLGTermsTableWdg', false);"
 				update=":clickLGTermsForm:clickLGTermsTable"
 				actionListener="#{geneView.fetchLossGainPointData()}" />
 			<p:remoteCommand name="fetchTimelinePointData"
 				update=":viewAnnotationsForm:viewAnnotationsTable" 
-				oncomplete="PF('viewAnnotationsDlgWdg').show()"
+				oncomplete="showViewAnnotationsDlg();"
 				action="#{geneView.fetchTimelinePointData()}" />
 				
 		</h:form>
@@ -202,7 +218,7 @@
 		<p:dialog id="timelineDlg" widgetVar="timelineDlgWdg" modal="true" showEffect="fade"
 			hideEffect="fade" height="800" width="1200"
 			fitViewport="true" closeOnEscape="true"
-			onHide="timelineDlgHide()">
+			onHide="timelineDlgHide();" onShow="">
 			<!-- return false so that the ajax request is not executed. -->
 			<p:ajax event="maximize"
 				onstart="timelineDlgResize();return false;" />
@@ -215,7 +231,7 @@
 		<p:dialog id="clickTermsDlg" header="Terms"
 			widgetVar="clickTermsDlgWdg" modal="false" showEffect="fade"
 			hideEffect="fade" width="900"
-			fitViewport="true" closeOnEscape="true">
+			fitViewport="true" closeOnEscape="true" onShow="handleDatatableDialog('clickTermsDlgWdg', 'clickTermsTableWdg', true);">
 			<h:form id="clickTermsForm">
 				<p:dataTable id="clickTermsTable" widgetVar="clickTermsTableWdg" var="term"
 					value="#{geneView.clickTerms}" scrollable="true" sortMode="multiple"
@@ -245,7 +261,7 @@
 						<h:outputText value="#{term.name}" />
 					</p:column>
 			        <p:column style="width:32px;text-align: center">
-			             <p:commandButton action="#{geneView.fetchAnnotations()}" update=":viewAnnotationsForm:viewAnnotationsTable" oncomplete="PF('viewAnnotationsDlgWdg').show()" icon="ui-icon-search" title="View">
+			             <p:commandButton action="#{geneView.fetchAnnotations()}" update=":viewAnnotationsForm:viewAnnotationsTable" oncomplete="showViewAnnotationsDlg();" icon="ui-icon-search" title="View">
 			                <f:setPropertyActionListener value="#{term}" target="#{geneView.viewTerm}" />
 			            </p:commandButton>
 			        </p:column>
@@ -259,7 +275,7 @@
 		<p:dialog id="clickLGTermsDlg" header="Terms"
 			widgetVar="clickLGTermsDlgWdg" modal="false" showEffect="fade"
 			hideEffect="fade" width="900"
-			fitViewport="true" closeOnEscape="true">
+			fitViewport="true" closeOnEscape="true" onShow="handleDatatableDialog('clickLGTermsDlgWdg', 'clickLGTermsTableWdg', true);">
 			<h:form id="clickLGTermsForm">
 				<p:dataTable id="clickLGTermsTable" widgetVar="clickLGTermsTableWdg" var="lg"
 					value="#{geneView.clickLGTerms}" scrollable="true" sortMode="multiple"
@@ -302,7 +318,7 @@
 						<h:outputText value="#{lg.term.name}" />
 					</p:column>
 			        <p:column style="width:32px;text-align: center">
-			             <p:commandButton action="#{geneView.fetchAnnotations()}" update=":viewAnnotationsForm:viewAnnotationsTable" oncomplete="PF('viewAnnotationsDlgWdg').show()" icon="ui-icon-search" title="View">
+			             <p:commandButton action="#{geneView.fetchAnnotations()}" update=":viewAnnotationsForm:viewAnnotationsTable" oncomplete="showViewAnnotationsDlg();" icon="ui-icon-search" title="View">
 			                <f:setPropertyActionListener value="#{lg.term}" target="#{geneView.viewTerm}" />
 			            </p:commandButton>
 			        </p:column>

gotrack/src/main/webapp/index.xhtml
@@ -90,21 +90,41 @@
 
 						<div class="section-title">Examples</div>
 
-				<p:lightBox styleClass="imagebox" id="lighbox1"> 
-					
+				<p:lightBox styleClass="imagebox"> 
 				    <h:outputLink value="/gotrack/javax.faces.resource/top_25.png?ln=img" title="Enrichment Top 25">  
 				    	<h:graphicImage library="img" width="100" height="56" name="top_25.png" />  
 				    </h:outputLink>
+				 
+				    <f:facet name="inline">
+				        <h:graphicImage library="img" width="900" name="top_25.png" />    
+				    </f:facet>
+				</p:lightBox>
+				<p:lightBox styleClass="imagebox"> 
 				    <h:outputLink value="/gotrack/javax.faces.resource/current_similarity.png?ln=img" title="Similarity Analysis">  
 				    	<h:graphicImage library="img" width="100" height="56" name="current_similarity.png" />  
 				    </h:outputLink>
+				 
+				    <f:facet name="inline">
+				        <h:graphicImage library="img" width="900" name="current_similarity.png" />   
+				    </f:facet>
+				</p:lightBox>
+				<p:lightBox styleClass="imagebox"> 
 				    <h:outputLink value="/gotrack/javax.faces.resource/RB1-Details.png?ln=img" title="Gene Tracking Details">  
 				    	<h:graphicImage library="img" width="100" height="56" name="RB1-Details.png" />  
 				    </h:outputLink>
+				 
+				    <f:facet name="inline">
+				        <h:graphicImage library="img" width="900" name="RB1-Details.png" />  
+				    </f:facet>
+				</p:lightBox>
+				<p:lightBox styleClass="imagebox"> 
 				    <h:outputLink value="/gotrack/javax.faces.resource/FOXP2-Evidence.png?ln=img" title="Gene Tracking Evidence">  
 				    	<h:graphicImage library="img" width="100" height="56" name="FOXP2-Evidence.png" />  
 				    </h:outputLink>
-				    
+				 
+				    <f:facet name="inline">
+				        <h:graphicImage library="img" width="900" name="FOXP2-Evidence.png" />  
+				    </f:facet>
 				</p:lightBox>
 				</div>
 				</div>

gotrack/src/main/webapp/resources/css/common.css
@@ -64,12 +64,24 @@ div.ui-terminal {
 	color: white;
 }
 
-button.icon-only {
-	height: 0px;
-	width: 0px;
-	top: 14px;
-	left: 5px;
+button.ui-button.icon-only {
+	background:none;
+	border:none;
+	width:16px;
 }
+
+button.ui-button.icon-only:focus {
+	outline:none;
+}
+
+button.ui-button.icon-only span {
+	left:0;
+}
+
+span.ui-column-title > button.ui-button.icon-only span {
+	margin-top: -12px;
+}
+	
 
 #body tr.ui-state-highlight td {
 	text-shadow: none;
@@ -119,4 +131,9 @@ button.icon-only {
 
 i.fa.fa-external-link:hover {
 	color: #0000ff;
+}
+
+table.no-border tr, 
+table.no-border td {
+	border: none !important;
 }

gotrack/src/main/webapp/resources/css/enrichment.css
@@ -107,10 +107,6 @@ span.filter-spinner input {
 	width:75px;
 }
 
-body .ui-selectonemenu .ui-selectonemenu-label {
-	width:auto;
-}
-
 div.data-grid-height-restricted > div.ui-datagrid-content {
   height: 300px;
   overflow-y: auto;

gotrack/src/main/webapp/resources/css/genes.css
@@ -20,6 +20,7 @@ table.geneSearchTable {
 
 div.ui-datatable button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-icon-only {
 	height: 16px;
+	outline: none;
 }
 
 tr.LOSS-color {

gotrack/src/main/webapp/resources/css/style.css
@@ -722,17 +722,20 @@ div.simpleCarousel .ui-carousel-header{
 	padding: 5px 15px;
 }
 
-#lighbox1 td {
+div.imagebox {
+	display:inline;
+}
+
+div.imagebox  td {
 	background-color: #e1e1e1;
 }
 
-#lighbox1 img {
+div.imagebox  img {
 	margin: 0 0 15px 15px;
 	border-style: solid;
 	border-width: 4px;
 	padding:5px;
 	border-color: #e1e1e1;
-	
 }
 
 div.autocomplete-largepanel {

commit 4b8a4d2ca87f015e7518cfe8a0c2f3b2cf9ee03f

Fixes bug in creating stability plot confidence interval
Introduced in f58d99c, heavy handed replacements.

gotrack/src/main/webapp/resources/js/enrichment.js
@@ -688,6 +688,8 @@ function handleGraphSelected(xhr, status, args) {
 
       for (var j = 0; j < series.data.length; j++) {
          var point = series.data[j];
+         // We disabled markers on the point level because disabling it at the series level removes series symbols from the legend.
+         // This is the least intrusive way of getting around this.
          data.push({x:point.x,y:utility.isUndefined( point.y ) ? null : point.y, marker:{enabled:false}});
       }
       options.series.push({
@@ -706,9 +708,7 @@ function handleGraphSelected(xhr, status, args) {
 
       for (var j = 0; j < series.data.length; j++) {
          var point = series.data[j];
-         // We disabled markers on the point level because disabling it at the series level removes series symbols from the legend.
-         // This is the least intrusive way of getting around this.
-         data.push({x:point.x,y:utility.isUndefined( point.y ) ? null : point.y, marker:{enabled:false}});
+         data.push([point.x,point.y.left, point.y.right]);
       }
       
       options.series.push({