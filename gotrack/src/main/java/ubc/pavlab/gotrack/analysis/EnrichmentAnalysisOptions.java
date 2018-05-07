a7133de753a7f49bec848663018192d121e51ea5
JacobsonMT <JacobsonMT+github@gmail.com>
2018-05-07 13:29:38 -0700

    Increase default enrichment min size to 20 from 5

--- gotrack/src/main/java/ubc/pavlab/gotrack/analysis/EnrichmentAnalysisOptions.java
+++ gotrack/src/main/java/ubc/pavlab/gotrack/analysis/EnrichmentAnalysisOptions.java
@@ -16,7 +16,7 @@ import java.util.List;
 @NoArgsConstructor
 public class EnrichmentAnalysisOptions {
 
-    private int minAnnotatedPopulation = 5; // minimum geneset size a specific term must have to be included in results
+    private int minAnnotatedPopulation = 20; // minimum geneset size a specific term must have to be included in results
     private int maxAnnotatedPopulation = 200; // maximum geneset size a specific term must have to be included in results
     private MultipleTestCorrection multipleTestCorrection = MultipleTestCorrection.BH; // method of multiple tests correction
     private double threshold = 0.05; // tEither p-value cutoff if using Bonferroni or FDR level if using BH step-up
